7190cb524bf2c96fc97fd7ffb874cea90c180b96
irum-shah78 <irams4812@gmail.com>
2024-09-10 02:23:07 +0500

    home-ui

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, SafeAreaView } from 'react-native';
 
 const HomePage = () => {
+  const [selectedTab, setSelectedTab] = useState('Today');
+  const tabs = ['Today', 'Week', 'Month', 'Year'];
+
+  const data = [
+    {
+      id: 1,
+      icon: require('../../assets/icons/shopping-bag.png'),
+      name: 'Shopping',
+      description: 'Buy some grocery',
+      price: '$120',
+      time: '10:00 AM',
+      backgroundColor: '#FCEED4',
+      paddingTop: 2,
+      paddingBottom: 2,
+      paddingRight: 2,
+      paddingLeft: 2,
+    },
+    {
+      id: 2,
+      icon: require('../../assets/icons/recurring-bill.png'),
+      name: 'Subscription',
+      description: 'Disney+ Annual..',
+      price: '$80',
+      time: '03:30 PM',
+      backgroundColor: '#fbe9e7',
+    },
+    {
+      id: 3,
+      icon: require('../../assets/icons/restaurant.png'),
+      name: 'Food',
+      description: 'Buy a ramen',
+      price: '$32',
+      time: '07:30 PM',
+      backgroundColor: '#fbe9e7',
+    },
+  ];
   return (
     <SafeAreaView style={styles.container}>
       {/* Header */}
@@ -9,33 +45,43 @@ const HomePage = () => {
         <View style={styles.overlayTop} />
         <View style={styles.overlayBottom} />
         <View style={styles.profileSection}>
-          <Image source={require('../../assets/images/profile.png')} style={styles.profileImage} />
+          <View style={styles.border}>
+            <Image source={require('../../assets/images/profile.png')} style={styles.profileImage} />
+          </View>
           <View>
-          <TouchableOpacity style={styles.droppDown}>
-          <Image source={require('../../assets/icons/arrow-down-month.png')} />
-            <Text style={styles.monthText}>October</Text>
-          </TouchableOpacity>
+            <TouchableOpacity style={styles.droppDown}>
+              <Image source={require('../../assets/icons/arrow-down-month.png')} />
+              <Text style={styles.monthText}>October</Text>
+            </TouchableOpacity>
           </View>
           <TouchableOpacity style={styles.notificationIcon}>
-          <Image source={require('../../assets/icons/notifiaction.png')} />
-        </TouchableOpacity>
+            <Image source={require('../../assets/icons/notifiaction.png')} />
+          </TouchableOpacity>
         </View>
-      <View style={styles.accountBalanceSection}>
-        <Text style={styles.accountBalanceText}>Account Balance</Text>
-        <Text style={styles.balanceAmount}>$9400</Text>
-        <View style={styles.overviewSection}>
-          <View style={styles.overviewBox1}>
-            <Image source={require('../../assets/icons/Income.png')} style={styles.overviewIcon} />
-            <Text style={styles.incomeText}>Income</Text>
-            <Text style={styles.amountText}>$5000</Text>
-          </View>
-          <View style={styles.overviewBox2}>
-            <Image source={require('../../assets/icons/Expense.png')} style={styles.overviewIcon} />
-            <Text style={styles.expenseText}>Expenses</Text>
-            <Text style={styles.amountText}>$1200</Text>
+        <View style={styles.accountBalanceSection}>
+          <Text style={styles.accountBalanceText}>Account Balance</Text>
+          <Text style={styles.balanceAmount}>$9400</Text>
+          <View style={styles.overviewSection}>
+            <View style={styles.overviewBox1}>
+              <View style={styles.incomeImage}>
+                <Image source={require('../../assets/icons/Income.png')} />
+              </View>
+              <View>
+                <Text style={styles.incomeText}>Income</Text>
+                <Text style={styles.amountText}>$5000</Text>
+              </View>
+            </View>
+            <View style={styles.overviewBox2}>
+              <View style={styles.incomeImage}>
+                <Image source={require('../../assets/icons/Expense.png')} />
+              </View>
+              <View>
+                <Text style={styles.expenseText}>Expenses</Text>
+                <Text style={styles.amountText}>$1200</Text>
+              </View>
+            </View>
           </View>
         </View>
-      </View>
       </View>
 
       <View style={styles.graphSection}>
@@ -45,24 +91,46 @@ const HomePage = () => {
 
       {/* Tabs */}
       <View style={styles.tabsSection}>
-        {['Today', 'Week', 'Month', 'Year'].map((tab) => (
-          <TouchableOpacity key={tab} style={styles.tab}>
-            <Text style={styles.tabText}>{tab}</Text>
+        {tabs.map((tab) => (
+          <TouchableOpacity
+            key={tab}
+            style={[
+              styles.tab,
+              selectedTab === tab ? styles.activeTab : styles.inactiveTab,
+            ]}
+            onPress={() => setSelectedTab(tab)}
+          >
+            <Text
+              style={[
+                styles.tabText,
+                selectedTab === tab ? styles.activeTabText : styles.inactiveTabText,
+              ]}
+            >
+              {tab}
+            </Text>
           </TouchableOpacity>
         ))}
       </View>
 
       {/* Recent Transactions */}
-      <View style={styles.recentTransactionsSection}>
-        <Text style={styles.recentTransactionsTitle}>Recent Transactions</Text>
-        <TouchableOpacity>
-          <Text style={styles.seeAllText}>See All</Text>
-        </TouchableOpacity>
-        {/* <ScrollView style={styles.transactionsList}>
-          <TransactionItem title="Shopping" description="Buy some grocery" amount="- $120" />
-          <TransactionItem title="Subscription" description="Disney+ Annual.." amount="- $80" />
-          <TransactionItem title="Food" description="Buy a ramen" amount="- $32" />
-        </ScrollView> */}
+      <View style={styles.transactionContainer}>
+        {data.map((item) => (
+          <View key={item.id} style={[styles.row]} >
+            <View style={styles.leftSection}>
+              <View style={[{ backgroundColor: item.backgroundColor, paddingTop: item.paddingTop,paddingBottom: item.paddingBottom,paddingLeft: item.paddingLeft,paddingRight: item.paddingRight}]}>
+                <Image source={item.icon} style={styles.icon} />
+              </View>
+              <View>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.description}>{item.description}</Text>
+              </View>
+            </View>
+            <View style={styles.rightSection}>
+              <Text style={styles.price}>{item.price}</Text>
+              <Text style={styles.time}>{item.time}</Text>
+            </View>
+          </View>
+        ))}
       </View>
     </SafeAreaView>
   );
@@ -71,10 +139,10 @@ const HomePage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 5,
+    paddingHorizontal: 3,
   },
   header: {
-    height: 312,
+    height: 270,
   },
   overlayTop: {
     ...StyleSheet.absoluteFillObject,
@@ -86,7 +154,7 @@ const styles = StyleSheet.create({
   overlayBottom: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: 'rgba(248, 237, 216, 0)',
-    top: '50%',
+    // top: '50%',
     borderBottomEndRadius: 30,
   },
   content: {
@@ -104,9 +172,18 @@ const styles = StyleSheet.create({
     paddingBottom: 8,
     paddingLeft: 16,
   },
+  border: {
+    borderWidth: 2,
+    borderRadius: 50,
+    paddingBottom: 2,
+    paddingLeft: 2,
+    paddingTop: 2,
+    paddingRight: 2,
+    borderColor: '#AD00FF',
+  },
   profileImage: {
-    width: 50,
-    height: 50,
+    width: 32,
+    height: 32,
     borderRadius: 25,
   },
   monthText: {
@@ -156,67 +233,66 @@ const styles = StyleSheet.create({
   overviewSection: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    gap: 10,
   },
   overviewBox1: {
-    width: '48%',
+    width: '50%',
     backgroundColor: '#00A86B',
-    borderRadius: 10,
+    borderRadius: 30,
     padding: 15,
     alignItems: 'center',
     flexDirection: 'row',
     paddingVertical: 15,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
   overviewBox2: {
-    width: '48%',
     backgroundColor: '#FD3C4A',
-    borderRadius: 10,
+    width: '50%',
+    borderRadius: 30,
     padding: 15,
     alignItems: 'center',
+    flexDirection: 'row',
+    paddingVertical: 15,
+    justifyContent: 'space-evenly',
   },
-  overviewIcon: {
-    width: 40,
-    height: 40,
+  incomeImage: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 10,
+  },
+  expenseImage: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 10,
   },
   incomeText: {
     color: '#FCFCFC',
     marginVertical: 5,
+    fontSize: 14,
   },
   expenseText: {
     color: '#FCFCFC',
     marginVertical: 5,
+    fontSize: 14,
   },
   amountText: {
-    fontSize: 18,
     fontWeight: '600',
     color: '#FCFCFC',
+    fontSize: 22,
   },
   graphSection: {
-    marginVertical: 20,
+    marginVertical: 10,
+    marginHorizontal: 5,
   },
   spendFrequencyText: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: '600',
+    color: '#0D0E0F',
   },
   graphImage: {
     width: '100%',
     height: 150,
     resizeMode: 'contain',
-  },
-  tabsSection: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginVertical: 10,
-  },
-  tab: {
-    paddingVertical: 8,
-    paddingHorizontal: 15,
-    borderRadius: 20,
-    backgroundColor: '#e1e1e1',
-  },
-  tabText: {
-    fontSize: 14,
-    fontWeight: '600',
   },
   recentTransactionsSection: {
     marginTop: 20,
@@ -231,19 +307,71 @@ const styles = StyleSheet.create({
     textAlign: 'right',
     marginVertical: 5,
   },
-  transactionsList: {
-    maxHeight: 200,
+  tabsSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  tab: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  activeTab: {
+    backgroundColor: '#FCEED4',
+  },
+  inactiveTab: {
+    backgroundColor: 'transparent',
+  },
+  tabText: {
+    fontSize: 16,
+  },
+  activeTabText: {
+    color: '#FCAC12',
+    fontWeight: 700,
+    fontSize: 14,
+  },
+  inactiveTabText: {
+    color: '#91919F',
+    fontWeight: 500,
+    fontSize: 14,
+  },
+  transactionContainer: {
+    padding: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  leftSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 60,
+  },
+  icon: {
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  description: {
+    fontSize: 14,
+    color: '#666',
+  },
+  rightSection: {
+    alignItems: 'flex-end',
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  time: {
+    fontSize: 14,
+    color: '#666',
   },
 });
-
-// const TransactionItem = ({ title, description, amount }) => (
-//   <View style={styles.transactionItem}>
-//     <View style={styles.transactionDetails}>
-//       <Text style={styles.transactionTitle}>{title}</Text>
-//       <Text style={styles.transactionDescription}>{description}</Text>
-//     </View>
-//     <Text style={styles.transactionAmount}>{amount}</Text>
-//   </View>
-// );
 
 export default HomePage;
